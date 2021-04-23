96019f0d0b350849b46840488b5b1b88f38c37d1
Selim Mecanna <mecannaselim@gmail.com>
2021-04-23 19:05:22 +0200

    Added get_valid_actions

--- game/connect4.py
+++ game/connect4.py
@@ -6,12 +6,25 @@ class Connect4:
     STATE_DEPTH = 5
 
     def __init__(self):
-        self.current_state = np.zeros((Connect4.STATE_DEPTH, *Connect4.BOARD_SIZE))
-        self.current_state[1] = np.ones(Connect4.BOARD_SIZE)
-        self.current_state[-2] = np.ones(Connect4.BOARD_SIZE)
+        self._current_state = np.zeros((Connect4.STATE_DEPTH, *Connect4.BOARD_SIZE))
+        self._current_state[1] = np.ones(Connect4.BOARD_SIZE)
+        self._current_state[-2] = np.ones(Connect4.BOARD_SIZE)
         self.visited_states = []
+
+    def get_valid_actions(self):
+        full_columns = self._current_state[0, 0, :] + self._current_state[2, 0, :]
+
+        return [not bool(s) for s in full_columns]
+
+    def is_terminated(self):
+
 
 
 if __name__ == "__main__":
     firstgame = Connect4()
-    print(firstgame.current_state)
+    print(firstgame._current_state)
+    print(firstgame.get_valid_actions())
+    firstgame._current_state[0, 0, 4:6] = 1
+    firstgame._current_state[2, 0, 2] = 1
+    print(firstgame._current_state)
+    print(firstgame.get_valid_actions())
