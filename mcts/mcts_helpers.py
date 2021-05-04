8e00493fb5ef7f5c47622bbf308e388ef32ce05e
Selim Mecanna <mecannaselim@gmail.com>
2021-05-04 14:11:48 +0200

    Implemented MCTS

---
+++ mcts/mcts_helpers.py
@@ -0,0 +1,52 @@
+from math import sqrt
+from game.connect4 import Connect4
+import random
+from mcts.tree import TreeNode
+
+
+# mcts_game = Connect4()
+# mcts_game.current_state = tree_node.state.copy()
+
+def simulate_mcts(tree_node, game, model):
+    game_status, return_value = game.is_terminated()
+    if game_status:
+        return -return_value
+
+    if not tree_node.children:
+        action = random.choice(list(tree_node.action_values))
+        game.play_move(action)
+        new_node = TreeNode(game, model)
+        tree_node.add_child(new_node, action)
+        tree_node.action_values[action] = (tree_node.action_values[action] * tree_node.passes[
+            action] - new_node.state_value) / (tree_node.passes[action] + 1)
+        tree_node.passes[action] += 1
+
+        return new_node.state_value
+
+    max_ucb = -float('inf')
+    picked_action = -1
+    cpuct = 4
+    for act in tree_node.action_values.keys():
+        ucb = tree_node.action_values[act] + cpuct * tree_node.policy[act](sqrt(sum(tree_node.passes.values))) / (
+                tree_node.passes[act] + 1)
+        if ucb > max_ucb:
+            max_ucb = ucb
+            picked_action = act
+
+    game.play_move(picked_action)
+
+    if picked_action in tree_node.children.keys():
+        new_node = tree_node.children[picked_action]
+
+    else:
+        new_node = TreeNode(game, model)
+        tree_node.add_child(new_node, picked_action)
+
+    v = simulate_mcts(new_node, game, model)
+
+    tree_node.action_values[picked_action] = (tree_node.action_values[picked_action] * tree_node.passes[
+        picked_action] + v) / (tree_node.passes[picked_action] + 1)
+
+    tree_node.passes[picked_action] += 1
+
+    return -v
--- mcts/tree.py
+++ mcts/tree.py
@@ -1,20 +1,19 @@
+import random
+
+
 class TreeNode:
     def __init__(self, game, model):
-        self.state = game.current_state
-        self.valid_actions = game.get_valid_actions
-        self.policy = model.predict(self.state)[1][*self.valid_actions]
-        self.state_value = model.predict(self.state)
+        self.state = game.current_state.copy()
+        # self.policy = model.predict(self.state)[1][*self.valid_actions]
+        # self.state_value = model.predict(self.state)
+        self.policy = {act: 1 / len(game.get_valid_actions()) for act in range(len(game.get_valid_actions()))}
+        self.state_value = random.random() * 2 - 1
 
-        valid_actions_number = sum(self.valid_actions)
-
-        self.action_values = [0]*valid_actions_number
-        self.passes = [0]*valid_actions_number
-        self.children = []
+        self.action_values = {act: 0 for act, val in enumerate(game.get_valid_actions()) if val}
+        self.passes = self.action_values.copy()
+        self.children = {}
         self.parent = None
-    
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
-    
 
-    
+    def add_child(self, child, action):
+        child.parent = self
+        self.children[action] = child
